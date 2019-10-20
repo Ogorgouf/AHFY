59c415c7f472b58198ceacb0baeab62899496676
donkene34 <nguimfackdilane@gmail.com>
2019-10-20 17:47:11 +0100

    'f'

--- include/header.php
+++ include/header.php
@@ -1,70 +1,130 @@
 <header align="center">
     <nav>
-        <div class="menu"> <a href="ahfy.php" class="les_liens <?php if($current == "ahfy"){ echo "current";} ?>">AHFY</a><div class='déroulant'>
-        <ul>
-                <li class="sous_liens"><div class="sous_div"><a class="lien"  href="">Listes des membres</a><div class="membres"></div> </div></li>
-                <hr>
-                <li class="sous_liens"><a class="lien" href="">connexion</a></li>
-                <hr>
-            </ul>
-        </div></div>
+        <div class="menu">
+            <a href="ahfy.php" class="les_liens <?php if($current == "ahfy"){ echo "current";} ?>">AHFY</a>
+            <div class='déroulant'>
+                <ul>
+                    <li class="sous_liens">
+                        <div class="sous_div">
+                            <a class="lien"  href="liste_membre.php">Listes des membres</a>
+                            <div class="membres"></div>
+                        </div>
+                    </li> <hr> 
+                    <li class="sous_liens"><a class="lien" href="">connexion</a></li> <hr> 
+                </ul>
+            </div>
+        </div>
        
-        <div class="menu"><a href="affy.php" class="les_liens  <?php if($current == "affy"){ echo "current";} ?>" >AFFY</a><div class='déroulant1'>
-        <ul>
-                <li class="sous_liens"><div class="sous_div"><a class="lien"  href="">Listes des membres</a><div class="membres"></div> </div></li>
-                <hr>
-                <li class="sous_liens"><a class="lien" href="">connexion</a></li>
-                <hr>
-            </ul>
-        </div></div>
-        <div class="menu"> <a href="aj.php" class="les_liens  <?php if($current == "aj"){ echo "current";} ?>">Association des jeunes</a><div class='déroulant2'>
-            <ul>
-                <li class="sous_liens"><div class="sous_div"><a class="lien"  href="">CECUSFO</a><div class="membres"></div> </div></li>
-                <hr>
-                <li class="sous_liens"><div class="sous_div"><a class="lien"  href="">génération 70</a><div class="membres1"></div> </div></li>
-                <hr>
-                <li class="sous_liens"><div class="sous_div"><a class="lien"  href="">génération 80</a><div class="membres2"></div> </div></li>
-                <hr>
-                <li class="sous_liens"><div class="sous_div"><a class="lien"  href="">génération 90</a><div class="membres3"></div> </div></li>
-                <hr>
-                <li class="sous_liens"><a class="lien" href="">connexion</a></li>
-                <hr>
-            </ul>
-        </div></div>
-        <div class="menu">  <a href="groupe_culturel.php" class="les_liens  <?php if($current == "groupe_culturel"){ echo "current";} ?>">Groupes culturels</a><div class='déroulant3'>
-        <ul>
-                <li class="sous_liens"><div class="sous_div"><a class="lien"  href="">LISSA</a><div class="membres"></div> </div></li>
-                <hr>
-                <li class="sous_liens"><div class="sous_div"><a class="lien"  href="">NKENAH</a><div class="membres1"></div> </div></li>
-                <hr>
-                <li class="sous_liens"><div class="sous_div"><a class="lien"  href="">MADZONG</a><div class="membres2"></div> </div></li>
-                <hr>
-              
-                <li class="sous_liens"><a class="lien" href="">connexion</a></li>
-                <hr>
-            </ul>
-        </div></div>
-       <div class="menu"> <a href="cercle_reflexion.php" class="les_liens  <?php if($current == "cercle_reflexion"){ echo "current";} ?>">Cercles de reflexions</a><div class='déroulant4'>
-       <ul>
-                <li class="sous_liens"><div class="sous_div"><a class="lien"  href="">CODEF</a><div class="membres"></div> </div></li>
-                <hr>
-                <li class="sous_liens"><div class="sous_div"><a class="lien"  href="">COREDEF</a><div class="membres1"></div> </div</li>
-                <hr>
-                <li class="sous_liens"><a class="lien" href="">connexion</a></li>
-                <hr>
-            </ul>
-       </div></div>
-       <div class="menu"> <a href="" class="les_liens">Autres</a><div class='déroulant5'>
-       <ul>
-                <li class="sous_liens"><div class="sous_div"><a class="lien"  href="">Mères d'enfants</a><div class="membres"></div> </div></li>
-                <hr>
-                <li class="sous_liens"><div class="sous_div"><a class="lien"  href="">Femmes dynamiques</a><div class="membres1"></div> </div></li>
-                <hr>
-               
-              
-                <li class="sous_liens"><a class="lien" href="">connexion</a></li>
-                <hr>
-            </ul>
-       </div></div>
+        <div class="menu">
+            <a href="affy.php" class="les_liens  <?php if($current == "affy"){ echo "current";} ?>" >AFFY</a>
+            <div class='déroulant1'>
+                <ul>
+                    <li class="sous_liens">
+                        <div class="sous_div">
+                            <a class="lien"  href="">Listes des membres</a>
+                            <div class="membres"></div> 
+                        </div>
+                    </li>  <hr>
+                    <li class="sous_liens"><a class="lien" href="">connexion</a></li> <hr>
+                </ul>
+            </div>
+        </div>
+
+        <div class="menu">
+            <a href="aj.php" class="les_liens  <?php if($current == "aj"){ echo "current";} ?>">Association des jeunes</a>
+            <div class='déroulant2'>
+                <ul>
+                    <li class="sous_liens">
+                        <div class="sous_div"><a class="lien"  href="">CECUSFO</a><div class="membres"></div>
+                        </div>
+                    </li> <hr>
+                    <li class="sous_liens">
+                        <div class="sous_div">
+                            <a class="lien"  href="">génération 70</a>
+                            <div class="membres1"></div>
+                        </div>
+                    </li> <hr>
+                    <li class="sous_liens">
+                        <div class="sous_div">
+                            <a class="lien"  href="">génération 80</a>
+                            <div class="membres2"></div>
+                        </div>
+                    </li>  <hr>
+                    <li class="sous_liens">
+                        <div class="sous_div">
+                            <a class="lien"  href="">génération 90</a>
+                            <div class="membres3"></div>
+                        </div></li>  <hr>
+                    <li class="sous_liens"><a class="lien" href="">connexion</a></li>  <hr>
+                </ul>
+            </div>
+        </div>
+
+        <div class="menu">
+            <a href="groupe_culturel.php" class="les_liens  <?php if($current == "groupe_culturel"){ echo "current";} ?>">Groupes culturels</a>
+            <div class='déroulant3'>
+                <ul>
+                    <li class="sous_liens">
+                        <div class="sous_div">
+                            <a class="lien"  href="">LISSA</a>
+                            <div class="membres"></div>
+                        </div>
+                    </li>  <hr>
+                    <li class="sous_liens">
+                        <div class="sous_div">
+                            <a class="lien"  href="">NKENAH</a>
+                            <div class="membres1"></div>
+                        </div>
+                    </li>  <hr>
+                    <li class="sous_liens">
+                        <div class="sous_div">
+                            <a class="lien"  href="">MADZONG</a>
+                            <div class="membres2"></div>
+                        </div>
+                    </li> <hr>
+                    <li class="sous_liens"><a class="lien" href="">connexion</a></li> <hr>
+                </ul>
+            </div>
+        </div>
+       <div class="menu">
+           <a href="cercle_reflexion.php" class="les_liens  <?php if($current == "cercle_reflexion"){ echo "current";} ?>">Cercles de reflexions</a>
+           <div class='déroulant4'>
+                <ul>
+                    <li class="sous_liens">
+                        <div class="sous_div">
+                            <a class="lien"  href="">CODEF</a>
+                            <div class="membres"></div>
+                        </div>
+                    </li>  <hr>
+                    <li class="sous_liens">
+                        <div class="sous_div">
+                            <a class="lien"  href="">COREDEF</a>
+                            <div class="membres1"></div>
+                        </div>
+                    </li> <hr>
+                    <li class="sous_liens"><a class="lien" href="">connexion</a></li> <hr>
+                </ul>
+            </div>
+        </div>
+       <div class="menu">
+           <a href="" class="les_liens">Autres</a>
+           <div class='déroulant5'>
+                <ul>
+                    <li class="sous_liens">
+                        <div class="sous_div">
+                            <a class="lien"  href="">Mères d'enfants</a>
+                            <div class="membres"></div>
+                        </div>
+                    </li>  <hr>
+                    <li class="sous_liens">
+                        <div class="sous_div">
+                            <a class="lien"  href="">Femmes dynamiques</a>
+                            <div class="membres1"></div>
+                        </div>
+                    </li>  <hr>
+                    <li class="sous_liens"><a class="lien" href="">connexion</a></li><hr>
+                </ul>
+            </div>
+        </div>
     </nav>
 </header>
--- view/liste_membre.view.php
+++ view/liste_membre.view.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <?php $current = 1; ?> 
+    <?php $current ='ahfy'; ?> 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php include("include/link.php") ?>
@@ -14,20 +14,12 @@
 <?php include("include/header.php")?>
 
 <section class="liste_des_membres">
-    <div class="entete_liste">
-         <div class="titre_liste_membre">membres de l' AFY</div>
-         <div class="admin">connexion&gt;</div>
-         <div class="form">
-            <form action="" method="POST">
-                <input class="input_email_mdp" type="email" name="email" placeholder="email..."><br><br><br>
-                <input class="input_email_mdp" type="password" name="mot_de_passe" placeholder="Mot De Passe"><br><br><br>
-                <input class="submit" type="submit" value="se connecter" name="connexion">
-            </form>
-         </div>
-    </div>
+    <div class="titre_liste">membres de l' AFY</div>
     <div class="liste_membre">
         <div class="liste">
-            <div class="photo_de_profil">photo</div>
+            <div class="div_photo_profil">
+                <div class="photo_de_profil">photo</div>
+            </div>
             <div class="nom">
                 <p> nom :<i><strong>DONFACK NGUIMFACK </strong></i></p>
                 <p> prenom : <i><strong>DILANE</strong></i> </p><br><br>
@@ -35,7 +27,9 @@
             </div>
         </div>
         <div class="liste">
-            <div class="photo_de_profil">photo</div>
+            <div class="div_photo_profil">
+                <div class="photo_de_profil">photo</div>
+            </div>
             <div class="nom">
                 <p> nom :<i><strong>DONFACK NGUIMFACK </strong></i></p>
                 <p> prenom : <i><strong>DILANE</strong></i> </p><br><br>
@@ -43,7 +37,9 @@
             </div>
         </div>
         <div class="liste">
-            <div class="photo_de_profil">photo</div>
+            <div class="div_photo_profil">
+                <div class="photo_de_profil">photo</div>
+            </div>
             <div class="nom">
                 <p> nom :<i><strong>DONFACK NGUIMFACK </strong></i></p>
                 <p> prenom : <i><strong>DILANE</strong></i> </p><br><br>
@@ -51,13 +47,26 @@
             </div>
         </div>
         <div class="liste">
-            <div class="photo_de_profil">photo</div>
+            <div class="div_photo_profil">
+                <div class="photo_de_profil">photo</div>
+            </div>
             <div class="nom">
                 <p> nom :<i><strong>DONFACK NGUIMFACK </strong></i></p>
                 <p> prenom : <i><strong>DILANE</strong></i> </p><br><br>
                 <p> statut: <I><strong>devellopeur web chez digittouch</strong></I></p>
             </div>
         </div>
+        <div class="liste">
+            <div class="div_photo_profil">
+                <div class="photo_de_profil">photo</div>
+            </div>
+            <div class="nom">
+                <p> nom :<i><strong>DONFACK NGUIMFACK </strong></i></p>
+                <p> prenom : <i><strong>DILANE</strong></i> </p><br><br>
+                <p> statut: <I><strong>devellopeur web chez digittouch</strong></I></p>
+            </div>
+        </div>
+       
     </div>
 </section>
 
