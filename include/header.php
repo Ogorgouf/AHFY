147b7ca71bf50b99f9eb83c537bfb681a08486b1
manshaftsantos <manshaftsantos@gmail.com>
2019-10-20 14:56:32 +0200

    c

--- include/header.php
+++ include/header.php
@@ -1,10 +1,10 @@
 <header align="center">
     <nav>
-        <a href="" class="les_liens current">AHFY</a>
-        <a href="" class="les_liens">AFFY</a>
-        <a href="" class="les_liens">Association des jeunes</a>
-        <a href="" class="les_liens">Groupes culturels</a>
-        <a href="" class="les_liens">Cercles de reflexions</a>
+        <a href="ahfy.php" class="les_liens <?php if($current == "ahfy"){ echo "current";} ?>">AHFY</a>
+        <a href="affy.php" class="les_liens  <?php if($current == "affy"){ echo "current";} ?>" >AFFY</a>
+        <a href="aj.php" class="les_liens  <?php if($current == "aj"){ echo "current";} ?>">Association des jeunes</a>
+        <a href="groupe_culturel.php" class="les_liens  <?php if($current == "groupe_culturel"){ echo "current";} ?>">Groupes culturels</a>
+        <a href="cercle_reflexion.php" class="les_liens  <?php if($current == "cercle_reflexion"){ echo "current";} ?>">Cercles de reflexions</a>
         <a href="" class="les_liens">Autres</a>
     </nav>
 </header>
