888a67369c78db8d95ddabbc202a36c037b0450e
YarlOverdose <tapamow@gmail.com>
2019-10-20 16:57:08 +0100

    lol

--- include/header.php
+++ include/header.php
@@ -1,6 +1,7 @@
 <header align="center">
     <nav>
-        <a href="ahfy.php" class="les_liens <?php if($current == "ahfy"){ echo "current";} ?>">AHFY</a>
+        <a href="index.php" class="logoafy"> <img src="img/logoafy.png" title="page d'acceuil" alt="logoafy" ></a>
+        <a href="ahfy.php" class="les_liens  <?php if($current == "ahfy"){ echo "current";} ?>">AHFY</a>
         <a href="affy.php" class="les_liens  <?php if($current == "affy"){ echo "current";} ?>" >AFFY</a>
         <a href="aj.php" class="les_liens  <?php if($current == "aj"){ echo "current";} ?>">Association des jeunes</a>
         <a href="groupe_culturel.php" class="les_liens  <?php if($current == "groupe_culturel"){ echo "current";} ?>">Groupes culturels</a>
