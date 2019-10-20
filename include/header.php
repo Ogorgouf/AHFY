16bfcd223e3587515e933514fc4cbda2fc0650db
manshaftsantos <manshaftsantos@gmail.com>
2019-10-20 17:18:19 +0200

    c

--- include/header.php
+++ include/header.php
@@ -1,10 +1,24 @@
 <header align="center">
     <nav>
-        <a href="ahfy.php" class="les_liens <?php if($current == "ahfy"){ echo "current";} ?>">AHFY</a>
-        <a href="affy.php" class="les_liens  <?php if($current == "affy"){ echo "current";} ?>" >AFFY</a>
-        <a href="aj.php" class="les_liens  <?php if($current == "aj"){ echo "current";} ?>">Association des jeunes</a>
-        <a href="groupe_culturel.php" class="les_liens  <?php if($current == "groupe_culturel"){ echo "current";} ?>">Groupes culturels</a>
-        <a href="cercle_reflexion.php" class="les_liens  <?php if($current == "cercle_reflexion"){ echo "current";} ?>">Cercles de reflexions</a>
-        <a href="" class="les_liens">Autres</a>
+        <div class="menu"> <a href="ahfy.php" class="les_liens <?php if($current == "ahfy"){ echo "current";} ?>">AHFY</a><div class='déroulant'></div></div>
+       
+        <div class="menu"><a href="affy.php" class="les_liens  <?php if($current == "affy"){ echo "current";} ?>" >AFFY</a><div class='déroulant1'></div></div>
+        <div class="menu"> <a href="aj.php" class="les_liens  <?php if($current == "aj"){ echo "current";} ?>">Association des jeunes</a><div class='déroulant2'>
+            <ul>
+                <li class="sous_liens"><div class="sous_div"><a class="lien"  href="">CECUSFO</a><div class="membres"></div> </div></li>
+                <hr>
+                <li class="sous_liens"><div class="sous_div"><a class="lien"  href="">génération 70</a><div class="membres1"></div> </div</li>
+                <hr>
+                <li class="sous_liens"><div class="sous_div"><a class="lien"  href="">génération 80</a><div class="membres2"></div> </div</li>
+                <hr>
+                <li class="sous_liens"><div class="sous_div"><a class="lien"  href="">génération 90</a><div class="membres3"></div> </div</li>
+                <hr>
+                <li class="sous_liens"><a class="lien" href="">connexion</a></li>
+                <hr>
+            </ul>
+        </div></div>
+        <div class="menu">  <a href="groupe_culturel.php" class="les_liens  <?php if($current == "groupe_culturel"){ echo "current";} ?>">Groupes culturels</a><div class='déroulant3'></div></div>
+       <div class="menu"> <a href="cercle_reflexion.php" class="les_liens  <?php if($current == "cercle_reflexion"){ echo "current";} ?>">Cercles de reflexions</a><div class='déroulant4'></div></div>
+       <div class="menu"> <a href="" class="les_liens">Autres</a><div class='déroulant5'></div></div>
     </nav>
 </header>
